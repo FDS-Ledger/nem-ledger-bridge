4e6ef1e68ae28f43c69744da4ec8082454af049e
Tri Phan <tri.phan@fdssoft.com>
2020-10-15 15:38:15 +0700

    Update to work with NEM BOLOS app version 0.0.2
    - Change to new package flow control
    
    Signed-off-by: Tri Phan <tri.phan@fdssoft.com>

--- src/hw-app-nem.js
+++ src/hw-app-nem.js
@@ -8,7 +8,7 @@ import BIPPath from "bip32-path";
  * const nem = new Nem(transport);
    recognize networkId by bip32Path;
       "44'/43'/networkId'/walletIndex'/accountIndex'"
-      
+
    const bip32path_mijin_testnet = "44'/43'/144'/1'/2'"
    const bip32path_mijin_mainnet "44'/43'/96'/3'/1'"
    const bip32path_nem_mainnet = "44'/43'/104'/5'/1'"
@@ -16,6 +16,7 @@ import BIPPath from "bip32-path";
  */
 
 const MAX_CHUNK_SIZE = 255;
+const CLA_FIELD = 0xe0
 
 export default class Nem {
 
@@ -37,25 +38,30 @@ export default class Nem {
      * const { publicKey, address } = result;
      */
     async getAddress(path) {
-        const display = false;
+        const GET_ADDRESS_INS_FIELD = 0x02
+        const display = true;
         const chainCode = false;
         const ed25519 = true;
 
         const bipPath = BIPPath.fromString(path).toPathArray();
         const curveMask = ed25519 ? 0x80 : 0x40;
 
-        const cla = 0xe0;
-        const ins = 0x02;
-        const p1 = display ? 0x01 : 0x00;
-        const p2 = curveMask | (chainCode ? 0x01 : 0x00);
-        const data = Buffer.alloc(1 + bipPath.length * 4);
+        // APDU fields configuration
+        const apdu = {
+          cla: CLA_FIELD,
+          ins: GET_ADDRESS_INS_FIELD,
+          p1: display ? 0x01 : 0x00,
+          p2: curveMask | (chainCode ? 0x01 : 0x00),
+          data: Buffer.alloc(1 + bipPath.length * 4),
+        };
 
-        data.writeInt8(bipPath.length, 0);
+        apdu.data.writeInt8(bipPath.length, 0);
         bipPath.forEach((segment, index) => {
-            data.writeUInt32BE(segment, 1 + index * 4);
+            apdu.data.writeUInt32BE(segment, 1 + index * 4);
         });
 
-        const response = await this.transport.send(cla, ins, p1, p2, data);
+        // Response from Ledger
+        const response = await this.transport.send(apdu.cla, apdu.ins, apdu.p1, apdu.p2, apdu.data);
 
         const result = {};
         const addressLength = response[0];
@@ -76,29 +82,27 @@ export default class Nem {
      * const signature = await nem.signTransaction(bip32path, "0390544100000000000000008A440493150000009029ECB35BFB8D51833381AA7947B9A4A21BA83712F338054B190001005369676E2066726F6D204C6564676572204E616E6F20532E44B262C46CEABB852823000000000000");
      */
     async signTransaction(path, rawTxHex) {
+        const TX_INS_FIELD = 0x04;
         const bipPath = BIPPath.fromString(path).toPathArray();
         const rawTx = Buffer.from(rawTxHex, "hex");
-        const curveMask = 0x80;
+
+        const chainCode = false;
+        const ed25519 = true;
+
+        const curveMask = ed25519 ? 0x80 : 0x40;
 
         const apdus = [];
         let offset = 0;
-        let twiceTransfer;
-
-        //The length of APDU buffer is 255Bytes
-        if (rawTx.length > 446) {
-            throw { "message": "The transaction is too long." };
-        } else {
-            twiceTransfer = rawTx.length > 234 ? true : false;
-        }
 
         while (offset !== rawTx.length) {
             const maxChunkSize = offset === 0 ? MAX_CHUNK_SIZE - 1 - bipPath.length * 4 : MAX_CHUNK_SIZE;
             const chunkSize = offset + maxChunkSize > rawTx.length ? rawTx.length - offset : maxChunkSize;
+            // APDU fields configuration
             const apdu = {
-                cla: 0xe0,
-                ins: 0x04,
-                p1: offset === 0 ? 0x00 : 0x80,
-                p2: curveMask,
+                cla: CLA_FIELD,
+                ins: TX_INS_FIELD,
+                p1: offset === 0 ? (chunkSize < maxChunkSize ? 0x00 : 0x80) : chunkSize < maxChunkSize ? 0x01 : 0x81,
+                p2: curveMask | (chainCode ? 0x01 : 0x00),
                 data: offset === 0 ? Buffer.alloc(1 + bipPath.length * 4 + chunkSize) : Buffer.alloc(chunkSize)
             };
 
@@ -108,13 +112,9 @@ export default class Nem {
                     apdu.data.writeUInt32BE(segment, 1 + index * 4);
                 });
                 rawTx.copy(apdu.data, 1 + bipPath.length * 4, offset, offset + chunkSize);
-                if (!twiceTransfer) {
-                    apdu.p1 = 0x90;
-                }
             } else {
                 rawTx.copy(apdu.data, 0, offset, offset + chunkSize);
             }
-
             apdus.push(apdu);
             offset += chunkSize;
         }
@@ -123,12 +123,17 @@ export default class Nem {
         for (let apdu of apdus) {
             response = await this.transport.send(apdu.cla, apdu.ins, apdu.p1, apdu.p2, apdu.data);
         }
+        // if (response.toString() != CONTINUE_SENDING) {
+        //     console.log("handle ok")
+        //     h = response.toString("hex");
+        // }
+
 
         // the last 2 bytes are status code from the hardware
         //return response.slice(0, response.length - 2).toString("hex");
 
         let h = response.toString("hex");
-        
+
         return {
             signature: h.slice(0, 128),
             publicKey: rawTxHex.slice(32, 96),
